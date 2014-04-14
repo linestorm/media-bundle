cb5cc34b3d626dd19590a346ea9ce259f5be68af
Andy Thorne <contrabandvr@gmail.com>
2014-04-14 00:47:10 +0000

    requirejs port

---
+++ src/LineStorm/MediaBundle/Resources/public/js/media-list.js
@@ -0,0 +1,49 @@
+
+define(['jquery', 'bootstrap', '../../../bundles/linestormcms/js/api'], function ($, bs, api) {
+    var page = 1,
+        $container;
+
+    function requestPage(){
+        $.ajax({
+            url: $container.data('url'),
+            data: {
+                page: page,
+                limit: 50
+            },
+            type: 'GET',
+            dataType: 'json',
+            success: function(o){
+                $container.empty();
+                var html = '';
+                for(var i in o){
+                    var media = o[i];
+                    var prototype = $container.data('prototype');
+                    for(var p in media){
+                        var prop = media[p];
+                        var rgx = new RegExp("__"+p+"__", "gim");
+                        prototype = prototype.replace(rgx, prop);
+                    }
+                    html += prototype;
+                }
+
+                $container.append(html);
+            }
+        });
+    };
+
+    $(document).ready(function(){
+        $container = $('.media-container');
+
+        $('a.media-prev').on('click', function(){
+            if(page <= 0)
+                return;
+            --page;
+            requestPage();
+        });
+        $('a.media-next').on('click', function(){
+            ++page;
+            requestPage();
+        });
+        requestPage();
+    });
+});
---
+++ src/LineStorm/MediaBundle/Resources/public/js/media.js
@@ -0,0 +1,64 @@
+
+define(['jquery', 'bootstrap', 'dropzone', '../../../bundles/linestormcms/js/api'], function ($, bs, Dropzone, api) {
+
+    // setup dropzone
+    Dropzone.autoDiscover = false;
+
+    var $form;
+    var $dropzone;
+    var carrosselDropZone;
+
+    $(document).ready(function(){
+        $form = $('form[name="linestorm_cms_form_media"]');
+        $dropzone = $('.dropzone');
+
+        carrosselDropZone = new Dropzone($dropzone[0], {
+            url: $dropzone.data('url'),
+            maxFiles: 1,
+            acceptedFiles: 'image/*',
+            init: function(){
+                this.on("success", function(file, response) {
+                    if(file.xhr.status == 200){
+                        alert('An identical file already exists.');
+                    } else {
+                        $form.find('.img-preview > img').attr('src', response.src);
+                        $form.find('input[name*="[src]"]').val(response.src);
+                        $form.find('input[name*="[hash]"]').val(response.hash);
+                        $form.find('input[name*="[title]"]').val(response.title);
+                        $('.media-form').slideDown();
+                        $dropzone.hide();
+                    }
+                    this.removeFile(file);
+
+                });
+                this.on("error", function(file, response) {
+                    this.removeFile(file);
+                    alert("Cannot add file: "+response);
+                });
+            },
+            previewTemplate: $dropzone.data('prototype')
+        });
+
+        $form.on('submit', function(e){
+            e.preventDefault();
+            e.stopPropagation();
+            $('#FormErrors').slideUp(function(){ $(this).html(''); });
+            window.lineStorm.api.saveForm($form, function(on, status, xhr){
+                if(xhr.status === 200){
+                } else if(xhr.status === 201) {
+                } else {
+                }
+            }, function(e, status, ex){
+                if(e.status === 400){
+                    if(e.responseJSON){
+                    } else {
+                        alert(status);
+                    }
+                }
+            });
+
+            return false;
+        });
+    });
+
+});
