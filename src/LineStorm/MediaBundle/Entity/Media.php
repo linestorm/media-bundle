5aa17ec99a97b0c1856587a712f4412489f2c76f
Andy Thorne <contrabandvr@gmail.com>
2014-06-02 16:53:00 +0000

    Initial commit of media categories

---
+++ src/LineStorm/MediaBundle/Entity/Media.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace LineStorm\MediaBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use LineStorm\MediaBundle\Model\Media as BaseMedia;
+
+/**
+ * Base Media Entity
+ *
+ * Class Media
+ *
+ * @package LineStorm\MediaBundle\Entity
+ */
+abstract class Media extends BaseMedia
+{
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    protected $id;
+
+    /**
+     * @var Media
+     *
+     * @ORM\OneToMany(targetEntity="Media", mappedBy="parent")
+     */
+    protected $children;
+
+    /**
+     * @var Media
+     *
+     * @ORM\ManyToOne(targetEntity="Media", inversedBy="children")
+     * @ORM\JoinColumn(name="parent_id", referencedColumnName="id")
+     */
+    protected $parent;
+
+    /**
+     * @var MediaCategory
+     *
+     * @ORM\ManyToOne(targetEntity="MediaCategory", inversedBy="media")
+     */
+    protected $category;
+
+} 
